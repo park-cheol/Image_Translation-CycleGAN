c697630981f31a557e740c997c71cd3023ef32d9
park-cheol <ekqqqus5555@gamil.com>
2021-05-01 20:42:41 +0900

    add model.py

--- model.py
+++ model.py
@@ -62,8 +62,10 @@ class Upsampling_Generator(nn.Module):
 
 class GeneratorResNet(nn.Module): # 이미지 사진 [channel, width ,height]
     def __init__(self, input_shape, num_residual_blocks):
+        # input_shpae = list
         super(GeneratorResNet, self).__init__()
         channels = input_shape[0]
+        self.num_residual_blocks = num_residual_blocks
 
         self.relu = nn.ReLU(inplace=True)
         self.tanh = nn.Tanh()
@@ -87,8 +89,8 @@ class GeneratorResNet(nn.Module): # 이미지 사진 [channel, width ,height]
                                                 padding=1)
 
         # Residual blcoks
-        for i in range(1, num_residual_blocks + 1):
-            locals()['residualLayer{}'.format(i)] = ResidualBlock(256)
+        for i in range(1, self.num_residual_blocks + 1):
+            locals()['self.residualLayer{}'.format(i)] = ResidualBlock(256)
 
         # Upsample
         self.upsample1 = Upsampling_Generator(in_channels=256,
@@ -107,28 +109,96 @@ class GeneratorResNet(nn.Module): # 이미지 사진 [channel, width ,height]
                                        )
 
     def forward(self, input): # input: [1, 3, 256, 256]
-        input = self.first_convlayer(input)
-        input = self.relu(input)
+        first_convLayer = self.first_convlayer(input)
+        first_convLayer_relu = self.relu(first_convLayer)
 
         #DownSample Layer
-        input = self.downsample1(input)
-        input = self.relu(input)
-        input = self.downsample2(input)
-        input = self.relu(input)
+        downsample_1 = self.downsample1(first_convLayer_relu)
+        downsample_1_relu = self.relu(downsample_1)
+        downsample_2 = self.downsample2(downsample_1_relu)
+        downsample_2_relu = self.relu(downsample_2)
 
-        # Residual Layer TODO 6BLOCK 9BLCOK일 때 방법
+        # Residual Layer
+        if self.num_residual_blocks == 6:
+            residualLayer1 = self.residualLayer1(downsample_2_relu)
+            residualLayer2 = self.residualLayer2(residualLayer1)
+            residualLayer3 = self.residualLayer3(residualLayer2)
+            residualLayer4 = self.residualLayer4(residualLayer3)
+            residualLayer5 = self.residualLayer5(residualLayer4)
+            residualLayer9 = self.residualLayer6(residualLayer5)
+
+        else: # residual_blocks 9개
+            residualLayer1 = self.residualLayer1(downsample_2_relu)
+            residualLayer2 = self.residualLayer2(residualLayer1)
+            residualLayer3 = self.residualLayer3(residualLayer2)
+            residualLayer4 = self.residualLayer4(residualLayer3)
+            residualLayer5 = self.residualLayer5(residualLayer4)
+            residualLayer6 = self.residualLayer6(residualLayer5)
+            residualLayer7 = self.residualLayer7(residualLayer6)
+            residualLayer8 = self.residualLayer8(residualLayer7)
+            residualLayer9 = self.residualLayer9(residualLayer8)
 
         # Upsample Layer
-        input = self.upsample1(input)
-        input = self.relu(input)
-        input = self.upsample2(input)
-        input = self.relu(input)
+        upsample1 = self.upsample1(residualLayer9)
+        upsample1_relu = self.relu(upsample1)
+        upsample2 = self.upsample2(upsample1_relu)
+        upsample2_relu = self.relu(upsample2)
 
         # last convolution layer
-        input = self.last_convlayer(input)
-        input = self.tanh(input)
+        last_convLayer = self.last_convlayer(upsample2_relu)
+        output = self.tanh(last_convLayer)
 
-        return input
+        return output
+
+##############################
+#       Discriminator
+##############################
+class Discriminator(nn.Module):
+    # 논문에서 70 x 70 patchGan 사용
+    # leakyrelu with a slope of 0.2
+    # 1-dimensional output
+    def __init__(self, input_shape):
+        super(Discriminator, self).__init__()
+        # input_shape (3, 256, 256)
+        channels, height, width = input_shape
+
+        # PatchGan 70 x 70
+        self.output_shape = (1, height // 2 ** 4, width // 2 ** 4)
+        # initial conv
+        self.leakyrelu = nn.LeakyReLU(0.2, inplace=True)
+        self.zeropad = nn.ZeroPad2d((1, 0, 1, 0))
+
+        self.convLayer_1 = nn.Sequential(nn.Conv2d(channels, 64, 4, stride=2, padding=1))
+
+        self.convLayer_2 = nn.Sequential(nn.Conv2d(64, 128, 4, stride=2, padding=1),
+                                         nn.InstanceNorm2d(128))
+
+        self.convLayer_3 = nn.Sequential(nn.Conv2d(128, 256, 4, stride=2, padding=1),
+                                         nn.InstanceNorm2d(256))
+
+        self.convLayer_4 = nn.Sequential(nn.Conv2d(256, 512, 4, stride=2, padding=1),
+                                         nn.InstanceNorm2d(512))
+
+        self.convLayer_5 = nn.Conv2d(512, 1, 4, padding=1)
+
+    def forward(self, input): # input[1, 3, 256, 256]
+        convLayer_1 = self.convLayer_1(input)
+        convLayer_1_relu = self.leakyrelu(convLayer_1)
+
+        convLayer_2 = self.convLayer_2(convLayer_1_relu)
+        convLayer_2_relu = self.leakyrelu(convLayer_2)
+
+        convLayer_3 = self.convLayer_3(convLayer_2_relu)
+        convLayer_3_relu = self.leakyrelu(convLayer_3)
+
+        convLayer_4 = self.convLayer_4(convLayer_3_relu)
+        convLayer_4_relu = self.leakyrelu(convLayer_4)
+
+        zeropad = self.zeropad(convLayer_4_relu)
+        output = self.convLayer_5(zeropad)
+
+        return output
+
 
 
 
